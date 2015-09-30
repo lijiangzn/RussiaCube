6dd45cb51443692f15d35f12d4f1e1f4b85aefc4
Jiang Li <ljang@amazon.com>
2015-09-30 16:04:55 +0800

    Initial Commit

---
+++ RussiaCube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RussiaCube
+//
+//  Created by Li, Jiang on 15/9/30.
+//  Copyright © 2015年 Li, Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
